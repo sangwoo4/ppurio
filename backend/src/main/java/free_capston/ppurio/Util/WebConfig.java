6feb4b34a3888e044b3ade6f997f254926493cfd
sangwoo4 <shangou97@gamil.com>
2024-11-04 00:35:12 +0900

    webconfig

---
+++ backend/src/main/java/free_capston/ppurio/Util/WebConfig.java
@@ -0,0 +1,25 @@
+package free_capston.ppurio.Util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000" ,"http://ppurio-1.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /app/uploads/ 경로를 실제 파일 시스템의 /Users/park/Desktop/git/backend/uploads/로 매핑합니다.
+        registry.addResourceHandler("/app/uploads/**")
+                .addResourceLocations("/uploads/");
+    }
