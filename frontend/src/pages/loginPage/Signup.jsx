f2653445417daff88ad2259c42c57749358764b8
Your Name <you@example.com>
2024-11-08 10:57:47 +0900

    로그인 회원가입 연동함

--- frontend/src/pages/loginPage/Signup.jsx
+++ frontend/src/pages/loginPage/Signup.jsx
@@ -4,7 +4,7 @@ import '../../styles/Signup.css';
 const Signup = () => {
   return (
     <div className="main-page">
-      <h1>회원가입 화면</h1>
+      <h1>회원가입 로직 - 안넣을 가능성 있음</h1>
     </div>
   );
 };
